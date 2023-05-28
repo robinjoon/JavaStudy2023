b64d754ddcf2059b9acbca77e6a45e0bcdc1e13b
robinjoon <robin980108@naver.com>
2023-05-29 00:54:24 +0900

    2주차 : 상위 클래스의 변수에 하위 클래스 인스턴스 대입

---
+++ src/main/java/week2/inheritance/type/Animal.java
@@ -0,0 +1,7 @@
+package week2.inheritance.type;
+
+public class Animal{
+    public void makeChild(){
+        System.out.println("자식을 만든다.");
+    }
+}
---
+++ src/main/java/week2/inheritance/type/Zoo.java
@@ -0,0 +1,7 @@
+package week2.inheritance.type;
+
+public class Zoo{
+    public static void main(String[] args){
+        Animal pingu = new Bird(); // 상위 클래스인 Animal로 선언된 변수 pingu 에 하위 클래스인 Bird 의 인스턴스를 대입.
+    }
+}

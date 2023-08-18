b8179aed55384849dc039119b29ca0fa74a3f002
robinjoon <robin980108@naver.com>
2023-08-19 01:09:29 +0900

    9주차 : 도서관 예제 재작성

--- src/main/java/oop/library/Main.java
+++ src/main/java/oop/library/Main.java
@@ -13,11 +13,21 @@ public class Main {
     public static void main(String[] args) {
         Library library = new SimpleLibrary(BookCalculator.getInstance(),BookCalculator.getInstance());
         Member member = new BasicMember(library);
+
         System.out.println(library.search("testBook"));
+
         Book testBook = library.getLendAbleBook("testBook");
         member.lendBook(testBook);
+
+        System.out.println(library.search("testBook"));
+
+        member.returnBook(testBook);
+
+        System.out.println(library.search("testBook"));
+
         Member member2 = new BasicMember(library);
         member2.lendBook(testBook);
+
         System.out.println(library.search("testBook"));
     }
 }
--- src/main/java/oop/library/library/SimpleLibrary.java
+++ src/main/java/oop/library/library/SimpleLibrary.java
@@ -30,11 +30,11 @@ public class SimpleLibrary implements Library {
         int allowLendCount = lendCountCalculator.calculateLendCount(member);
 
         if(lendList.size()>=allowLendCount){
-            return new LendResult(false, Period.ZERO,book);
+            throw new IllegalStateException("더이상 대출할 수 없습니다.");
         }
 
         if(!book.canLend()){
-            return new LendResult(false, Period.ZERO,book,"이미 대출중인 책입니다.");
+            throw new IllegalStateException("이미 대출중인 책입니다.");
         }
 
         book.lend(member);
@@ -72,25 +72,46 @@ public class SimpleLibrary implements Library {
 
     @Override
     public BookSearchResult search(String bookName) {
-        List<Book> searchedBooks = bookDB.getOrDefault(bookName,Collections.emptyList());
-        if (searchedBooks.isEmpty()){
-            return null;
+        int totalBookCount = getTotalBookCount(bookName);
+        int remainBookCount = getRemainBookCount(bookName);
+        if (!isKnownBook(bookName)){
+            return new BookSearchResult(bookName,0,0,null);
         }
+        if(canLendNow(remainBookCount)){
+            return new BookSearchResult(bookName,remainBookCount,totalBookCount,LocalDate.now());
+        }
+        List<LocalDate> returnDates = new ArrayList<>();
+        List<Book> searchedBooks = getBooks(bookName);
+        for (Book searchedBook : searchedBooks) {
+            returnDates.add(returnDateDB.get(searchedBook));
+        }
+        returnDates.sort(Comparator.naturalOrder());
+        return new BookSearchResult(bookName,remainBookCount,totalBookCount,returnDates.get(0));
+    }
+    private int getTotalBookCount(String bookName){
+        return bookDB.getOrDefault(bookName,Collections.emptyList()).size();
+    }
+
+    private int getRemainBookCount(String bookName){
+        List<Book> searchedBooks = bookDB.getOrDefault(bookName,Collections.emptyList());
         int remainCount = 0;
         for (Book searchedBook : searchedBooks) {
             if(searchedBook.canLend())remainCount++;
         }
-        if(remainCount == 0){
-            List<LocalDate> returnDates = new ArrayList<>();
-            for (Book searchedBook : searchedBooks) {
-                returnDates.add(returnDateDB.get(searchedBook));
-            }
-            returnDates.sort(Comparator.naturalOrder());
-            return new BookSearchResult(bookName,remainCount,searchedBooks.size(),returnDates.get(0));
-        }
-        return new BookSearchResult(bookName,remainCount,searchedBooks.size(),LocalDate.now());
+        return remainCount;
     }
 
+    private boolean isKnownBook(String bookName){
+        return getTotalBookCount(bookName)>0;
+    }
+
+    private List<Book> getBooks(String bookName){
+        return bookDB.getOrDefault(bookName,Collections.emptyList());
+    }
+
+    private boolean canLendNow(int remainCount){
+        return remainCount > 0;
+    }
     @Override
     public List<Book> searchLendList(Member member) {
         return lendDB.get(member);
@@ -100,11 +121,11 @@ public class SimpleLibrary implements Library {
     public Book getLendAbleBook(String bookName) {
         List<Book> books = bookDB.getOrDefault(bookName,Collections.emptyList());
         if(books.isEmpty()){
-            return null;
+            throw new IllegalArgumentException("구비중인 책이 아닙니다.");
         }
         for (Book book : books) {
             if (book.canLend())return book;
         }
-        return null;
+        throw new IllegalStateException("대출 가능한 책이 없습니다.");
     }
 }
